eb7c94b26125843eab34995e7f15ee1044f2df58
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-04-30 08:05:47 +0100

    chore(config): add Trackly root URL configuration

---
+++ config/url.py
@@ -0,0 +1,13 @@
+"""Root URL configuration for Trackly."""
+
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+
+urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="dashboard:user", permanent=False)),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.users.urls")),
+    path("dashboard/", include("apps.dashboard.urls")),
+]
