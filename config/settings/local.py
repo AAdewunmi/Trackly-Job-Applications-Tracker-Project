9b0c7fb0f4c61e8a3f3ae12c6f4150393fd0ba48
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-04-30 09:12:39 +0100

    chore(config): add local Django settings

---
+++ config/settings/local.py
@@ -0,0 +1,16 @@
+"""Local development settings for Trackly."""
+
+# from .base import *  # noqa: F403
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
