edb861280188e1b1268befc890a2ae01686220cb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-05-26 10:02:12 +0100

    docs(roles): clarify roles app configuration docstrings

--- apps/roles/apps.py
+++ apps/roles/apps.py
@@ -1,10 +1,10 @@
-"""Roles application configuration."""
+"""Application configuration for the roles app."""
 
 from django.apps import AppConfig
 
 
 class RolesConfig(AppConfig):
-    """Configure the roles app."""
+    """Configure the Trackly roles app."""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.roles"
