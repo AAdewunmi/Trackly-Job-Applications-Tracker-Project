c7a44a103f580b3f127be37ed3d8d49093572706
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-05-26 09:02:55 +0100

    fix(docs): update users app configuration docstring for clarity

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,10 +1,10 @@
-"""Users application configuration."""
+"""Application configuration for the users app."""
 
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    """Configure the users app."""
+    """Configure the Trackly users app."""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.users"
