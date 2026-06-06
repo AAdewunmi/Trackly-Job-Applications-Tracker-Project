a4b5a958088a6e010151d2c252666636b00cfc71
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-06-06 11:15:11 +0100

    chore(auth): add JWT token routes

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from config.views import home
 
@@ -13,6 +14,16 @@ urlpatterns = [
     path("accounts/", include("apps.users.urls")),
     path("applications/", include("apps.jobs.urls")),
     path("dashboard/", include("apps.dashboard.urls")),
+    path(
+        "api/v1/auth/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/v1/auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 ]
 
 if settings.DEBUG:
